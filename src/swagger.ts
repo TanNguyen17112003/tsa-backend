b2a26d19c00c3eae8284ea079796ab62458cf5e9
TanNguyen <duytan17112003@gmail.com>
2024-06-18 09:59:40 +0700

    chore: test cicd deployment

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,6 @@
+import { Express } from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
 
 const options = {
   definition: {
