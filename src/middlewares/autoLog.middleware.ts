685ff97dab2337667d30e3b6854849aa2e474d4c
TanNguyen <duytan17112003@gmail.com>
2024-06-21 00:26:34 +0700

    fix: remove logger utils

--- src/middlewares/autoLog.middleware.ts
+++ src/middlewares/autoLog.middleware.ts
@@ -1,26 +1,26 @@
-import { Request, Response, NextFunction } from 'express';
-import { authLogger, userLogger } from '../utils/logger';
+// import { Request, Response, NextFunction } from 'express';
+// import { authLogger, userLogger } from '../utils/logger';
 
-function authAutoLog(req: Request, res: Response, next: NextFunction) {
-  const originalJson = res.json;
-  res.json = function (body) {
-    const logLevel = res.statusCode >= 400 ? 'error' : 'info';
-    authLogger.log(logLevel, 'Response sent', { path: req.path, status: res.statusCode, body });
-    return originalJson.call(this, body);
-  };
+// function authAutoLog(req: Request, res: Response, next: NextFunction) {
+//   const originalJson = res.json;
+//   res.json = function (body) {
+//     const logLevel = res.statusCode >= 400 ? 'error' : 'info';
+//     authLogger.log(logLevel, 'Response sent', { path: req.path, status: res.statusCode, body });
+//     return originalJson.call(this, body);
+//   };
 
-  next();
-}
+//   next();
+// }
 
-function userAutoLog(req: Request, res: Response, next: NextFunction) {
-  const originalJson = res.json;
-  res.json = function (body) {
-    const logLevel = res.statusCode >= 400 ? 'error' : 'info';
-    userLogger.log(logLevel, 'Response sent', { path: req.path, status: res.statusCode, body });
-    return originalJson.call(this, body);
-  };
+// function userAutoLog(req: Request, res: Response, next: NextFunction) {
+//   const originalJson = res.json;
+//   res.json = function (body) {
+//     const logLevel = res.statusCode >= 400 ? 'error' : 'info';
+//     userLogger.log(logLevel, 'Response sent', { path: req.path, status: res.statusCode, body });
+//     return originalJson.call(this, body);
+//   };
 
-  next();
-}
+//   next();
+// }
 
-export { authAutoLog, userAutoLog };
+// export { authAutoLog, userAutoLog };
