d86367f2ddfc20bca2b49d94e81b37a11d65c66c
Quan Cao <quancao2310@gmail.com>
2024-09-29 22:24:55 +0700

    chore: remove sample controller, service and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth';
-import { AppController } from 'src/controllers/app.controller';
-import { SampleController } from 'src/controllers/sample.controller';
 import { DateModule } from 'src/date';
 import { EmailModule } from 'src/email';
 import { OrdersController } from 'src/models/orders/orders.controller';
 import { ReportsController } from 'src/models/reports/reports.controller';
 import { PrismaModule } from 'src/prisma';
-import { AppService } from 'src/services/app.service';
-import { SampleService } from 'src/services/sample.service';
 import { UsersModule } from 'src/users';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -21,7 +20,7 @@ import { UsersModule } from 'src/users';
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController, SampleController, OrdersController, ReportsController],
-  providers: [AppService, SampleService],
+  controllers: [AppController, OrdersController, ReportsController],
+  providers: [AppService],
 })
 export class AppModule {}
