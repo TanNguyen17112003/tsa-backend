ee4c31db0d875af5567f1ac53b18dce637e73c6b
TanNguyen <duytan17112003@gmail.com>
2024-10-17 09:30:56 +0700

    refactor: add mobule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
@@ -7,10 +6,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { DateModule } from './date/date.module';
+import { DeliveriesModule } from './deliveries';
 import { EmailModule } from './email/email.module';
 import { FirebaseAdminConfigService } from './firebase-admin.config';
 import { OrdersModule } from './orders/orders.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ReportsModule } from './reports';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -23,6 +24,8 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     OrdersModule,
     CloudinaryModule,
+    ReportsModule,
+    DeliveriesModule,
   ],
   controllers: [AppController],
   providers: [AppService, FirebaseAdminConfigService],
