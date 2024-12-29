06b48ceac1cd28a4acec5b857b17df3316b71925
Quan Cao <quancao2310@gmail.com>
2024-12-29 22:03:18 +0700

    feat: add basic stats about orders for staff and student

--- src/orders/dtos/index.ts
+++ src/orders/dtos/index.ts
@@ -1,5 +1,6 @@
 export * from './create.dto';
 export * from './query.dto';
 export * from './response.dto';
+export * from './stats.dto';
 export * from './update.dto';
 export * from './updateStatus.dto';
