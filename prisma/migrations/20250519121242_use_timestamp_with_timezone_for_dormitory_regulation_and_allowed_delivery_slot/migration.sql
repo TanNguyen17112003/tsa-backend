c7b7e61274d0676c040f8001a38c55bc25401fa6
Quan Cao <quancao2310@gmail.com>
2025-05-19 19:14:19 +0700

    fix: use timestamptz for db field of AllowedDeliverySlot and DormitoryRegulation

---
+++ prisma/migrations/20250519121242_use_timestamp_with_timezone_for_dormitory_regulation_and_allowed_delivery_slot/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "AllowedDeliverySlot" ALTER COLUMN "updateAt" SET DATA TYPE TIMESTAMPTZ(3);
+
+-- AlterTable
+ALTER TABLE "DormitoryRegulation" ALTER COLUMN "updateAt" SET DATA TYPE TIMESTAMPTZ(3);
